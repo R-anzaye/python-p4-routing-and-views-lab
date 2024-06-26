ab3e2248f61034064e4c2414be249e0315a874ba
Ryan Anzaye <rileydollar8@gmail.com>
2024-06-26 19:40:22 +0300

    change

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 from flask import Flask
 
@@ -7,34 +8,36 @@ app = Flask(__name__)
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-@app.route('/print/<string:parameter>')
-def print_string(parameter):
-    print(parameter)
-    return f'<h1>{parameter}</h1>'
+@app.route('/print/<string:route>')
+def print_string(route):
+    print(route)
+    return route
 
-@app.route('/count/<int:parameter>')
-def count(parameter):
-    numbers = ''.join(f'{i}<br>' for i in range(parameter))
-    return f'<h1>{numbers}</h1>'
-
+@app.route('/count/<int:number>')
+def count(number):
+    count = f''
+    for n in range(number):
+        count += f'{n}\n'
+    return count
 
 @app.route('/math/<int:num1>/<string:operation>/<int:num2>')
-def math(num1, operation, num2):
-    result = None
+def math(num1, num2, operation):
     if operation == '+':
-        result = num1 + num2
-    elif operation == '-':
-        result = num1 - num2
-    elif operation == '*':
-        result = num1 * num2
-    elif operation == 'div':
-        result = num1 / num2
-    elif operation == '%':
-        result = num1 % num2
-    else:
-        return '<h1>Invalid operation. Use +, -, *, div, or %.</h1>'
+        return str(num1 + num2)
     
-    return f'<h1>{num1} {operation} {num2} = {result}</h1>'
+    elif operation == '-':
+        return str(num1 - num2)
+
+    elif operation == '*':
+        return str(num1 * num2)
+
+    elif operation == 'div':
+        return str(num1 / num2)
+
+    elif operation == '%':
+        return str(num1 % num2)
+
+    return 'Operation not recognized. Please use one of the following: + - * div %'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
